717c71adf56c8d7ab0d9d4a4dc5046bb807c8ea6
maamir27 <97367606+maamir27@users.noreply.github.com>
2023-11-09 20:12:42 -0500

    image hosting is set up

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -2,10 +2,9 @@ class Outfit < ApplicationRecord
   belongs_to :user
   has_many :requests, dependent: :destroy
 
+  has_one_attached :photo
   # validates :photo_url, presence: true
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
-
-  has_one_attached :photo
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_08_010221) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_003430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
